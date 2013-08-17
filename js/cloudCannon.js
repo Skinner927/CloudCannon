f1a8f75804bfff97b9462684f9dfd93c3b2032ec
dskinner <dskinner@caci.com>
2013-08-17 17:33:36 -0400

    lolnoidea

--- js/cloudCannon.js
+++ js/cloudCannon.js
@@ -9,7 +9,7 @@ function CloudCannon(usr_options) {
     maxDuration: 10000,   // Max amount of time (ms) for cloud to cross screen
     fireDelay: 500,      // Time to wait after a cloud leaves the screen before shooting out a new one
     sky: $('#sky'),       // JQuery object that is the sky
-    cloudImg: '../img/cloud.gif', // Cloud image
+    cloudImg: 'img/cloud.gif', // Cloud image
     direction: 1, // 1 = right to left, -1 = left to right
     cloudScales: [1, 0.8, 0.5, 0.3],
     minAltitude: 600,     // Horizontal line from top of sky where clouds cannot go below (-1 is to edge)
@@ -26,12 +26,24 @@ function CloudCannon(usr_options) {
   self.clouds = 0;
   
   // Starts the cannon
-  self.start = function() {
-    self.shootCloud();
-    
+  self.start = function() {  
+    if(self.clouds == 0) {
+      $('<img src="'+self.options.cloudImg+'" />').load(function(){
+        // get the width and height of the cloud
+        self.options.cloudWidth = this.width;
+        self.options.cloudHeight = this.height;
+        // Start the cannon
+        self.cloudLoop();
+      });
+    }   
+  };
+  
+  // This is the main loop for generating clouds
+  self.cloudLoop = function(){
     // Keep firing if we don't have all the initial clouds out
     if(self.clouds < self.options.maxClouds) {
-      setTimeout(function(){self.start();}, self.options.fireDelay);
+      self.shootCloud();
+      setTimeout(function(){self.cloudLoop();}, self.options.fireDelay);
     }
   };
   
@@ -40,6 +52,12 @@ function CloudCannon(usr_options) {
     self.clouds += 1;    
     console.log('newCloud!', self.clouds);
     
+    var cloud = $('<img src="'+self.options.cloudImg+'" />');
+    cloud.css({
+      position: absolute;
+      left: (self.options.cloudWidth * -1) + 5,
+      top: (self.options.maxAltitude == -1) ? 0= : self.options.maxAltitude
+    });
     
   }
 }
