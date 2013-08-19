d2938c2a9beb8596adcb58d3507cd7fc33adf4bc
dskinner <dskinner@caci.com>
2013-08-19 14:34:37 -0400

    Working CloudCannon

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 CloudCannon
 ===========
+
+# Config #
+
+*Variable* => *(type) defaultValue* : *description*
+
+maxClouds => (int) 10 : max number of clouds to display on screen
+
+minDuration => (int) 10000 : minimum amount of time for a cloud to cross the sky in ms
+
+maxDuration => (int) 30000 : max amount of time for a cloud to cross the sky in ms
+
+fireDelay => (int) 1000 : time to wait between creating clouds
+
+sky => (JQuery Object) $('#sky') : JQuery object which represents the sky (where clouds will be rendered)
+
+cloudImg => (string) 'img/cloud.gif' : path (relative to html) to cloud gif
+
+direction => (string) 'left' : direction clouds move 'left' or 'right'
+
+cloudScales => (float array) [1, 0.8, 0.5, 0.3] : Array to randomly scale clouds
+
+maxAltitude => (int) -1 : Distance from top of sky clouds will render. -1 is the same as 0 and is the top of the sky.
+
+minAltitude => (int) -1 : Distance from top of sky clouds will render. -1 is the same as the bottom/floor/ground of the sky.
+
+zindex => (int) 100 : css z-index value for the clouds
+
+## Altitude Settings ##
+
+The above altitude settings can be confusing. 
+Please reference this awesome ascii art for reference.
+
+
++-- #sky ---------------------------------------------------+
+|           |                       |                       |
+|           | [distance from top]   |                       |
+|          \ /                      |                       |
+|---- maxAltitude ------------------+-----------------------|
+|                                   |                       |
+|                                   | [distance from top]   |
+|                                  \ /                      |
+|----------------------------- minAltitude -----------------|
+|                                                           |
+|___________________________________________________________|
--- js/cloudCannon.js
+++ js/cloudCannon.js
@@ -3,39 +3,52 @@
 function CloudCannon(usr_options) {
   var self = this;
   
+  self.random = function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  
   self.options = {
-    maxClouds: 10,        // Max number of clouds on screen at one time
-    minDuration: 5000,    // Min amount of time (ms) for cloud to cross screen
-    maxDuration: 10000,   // Max amount of time (ms) for cloud to cross screen
-    fireDelay: 500,      // Time to wait after a cloud leaves the screen before shooting out a new one
-    sky: $('#sky'),       // JQuery object that is the sky
-    cloudImg: 'img/cloud.gif', // Cloud image
-    direction: 1, // 1 = right to left, -1 = left to right
+    maxClouds: 10,              // Max number of clouds on screen at one time
+    minDuration: 10000,         // Min amount of time (ms) for cloud to cross screen
+    maxDuration: 30000,         // Max amount of time (ms) for cloud to cross screen
+    fireDelay: 1000,            // Time to wait after a cloud leaves the screen before shooting out a new one
+    sky: $('#sky'),             // JQuery object that is the sky
+    cloudImg: 'img/cloud.gif',  // Cloud image
+    direction: 'left',          // 'left' = left to right, 'right' = right to left
     cloudScales: [1, 0.8, 0.5, 0.3],
-    minAltitude: 600,     // Horizontal line from top of sky where clouds cannot go below (-1 is to edge)
-    maxAltitude: -1,      // Horizontal line from top of sky where clouds cannot go above (-1 is to edge)
+    minAltitude: -1,           // Distance from top of sky in pixels (-1 means to the floor). If you're setting this number, it should be higher than maxAltitude
+    maxAltitude: -1,           // Distance from top of sky in pixels (-1 is the same as 0)
+    zindex: 100,
     
-    cloudWidth: 500,       // DO NOT CHANGE: This value is recalculated on first run
-    cloudHeight: 500       // DO NOT CHANGE: This value is recalculated on first run
+    cloudWidth: 500,      // DO NOT CHANGE: This value is recalculated on first run
+    cloudHeight: 500      // DO NOT CHANGE: This value is recalculated on first run
   };
   
-  // TODO: let user override these options by passing them in
-  // usr_options
+  // Apply user settings
+  for (var key in usr_options){ 
+    if (self.options.hasOwnProperty(key))
+      self.options[key] = usr_options[key];
+  }  
   
   // Number of clouds in the sky
   self.clouds = 0;
   
   // Starts the cannon
   self.start = function() {  
+    // Init
     if(self.clouds == 0) {
       $('<img src="'+self.options.cloudImg+'" />').load(function(){
         // get the width and height of the cloud
         self.options.cloudWidth = this.width;
         self.options.cloudHeight = this.height;
+        
+        self.options.maxAltitude = (self.options.MaxAltitude < 1) ? 0 : self.options.maxAltitude;
+        self.options.minAltitude = (self.options.MinAltitude < 1) ? sky.height() : self.options.minAltitude;
+        
         // Start the cannon
         self.cloudLoop();
       });
-    }   
+    }
   };
   
   // This is the main loop for generating clouds
@@ -49,15 +62,43 @@ function CloudCannon(usr_options) {
   
   // Creates a new cloud and sends it on its way
   self.shootCloud = function() {
+    // Increment counter before we build
     self.clouds += 1;    
-    console.log('newCloud!', self.clouds);
     
-    var cloud = $('<img src="'+self.options.cloudImg+'" />');
+    // Resize ratio for cloud
+    var imageRatio = self.options.cloudScales[self.random(0, self.options.cloudScales.length - 1)];
+    
+    // Create the cloud
+    var cloud = $('<img src="'+self.options.cloudImg+'" />');       
+    // Starts the cloud off screen
+    cloud.css(self.options.direction, (self.options.cloudWidth * -1) - 5);  
+    
+    // Calculate the top to prevent the cloud from going outside the allowed altitude   
+    var top = self.random(self.options.maxAltitude, self.options.minAltitude - (self.options.cloudHeight * imageRatio));
+
+    // Standard CSS
     cloud.css({
-      position: absolute;
-      left: (self.options.cloudWidth * -1) + 5,
-      top: (self.options.maxAltitude == -1) ? 0= : self.options.maxAltitude
+      position: 'absolute',      
+      top: top,
+      width: self.options.cloudWidth * imageRatio,
+      height: self.options.cloudHeight * imageRatio,
+      zindex: self.options.zindex
     });
+        
+    // Stick the cloud in the sky
+    cloud.appendTo(self.options.sky);
+    
+    // Animation properties
+    var animation = {};             
+    animation[self.options.direction] = '+=' + (self.options.sky.width() + self.options.cloudWidth + 5);
+    
+    // Send the cloud on its way
+    cloud.animate(animation, self.random(self.options.minDuration, self.options.maxDuration), 'linear', function(){
+      $(this).remove();
+      self.clouds -= 1;
+      setTimeout(function(){self.cloudLoop();}, self.options.fireDelay);
+    });
+    
     
   }
 }
